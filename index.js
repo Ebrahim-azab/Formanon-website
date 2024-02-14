d87eabc3f179af836fb667156e841fa693cd0d1a
Ebrahim-azab <160005366+Ebrahim-azab@users.noreply.github.com>
2024-02-14 10:31:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function getUserInput() {
+    // Prompt the user for input
+    let userInput = prompt("Please enter your input:");
+
+    // Return the user input
+    return userInput;
+}
+
+// Example usage
+let userInput = getUserInput();
+console.log("User input:", userInput);
